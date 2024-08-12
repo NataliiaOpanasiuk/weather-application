084a541507baa1dbfd8f6272e9dc22de05615558
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-08-12 15:40:59 -0700

    Cleaned code for better reading

--- src/index.js
+++ src/index.js
@@ -1,4 +1,61 @@
-// Date
+// ---------------------------------------------------------------------------      Current weather
+//---------------------------------------------------------------------------
+
+// API call, getting city name and current temperature---------------------
+function showCityData(response) {
+  clickCount = 0;
+
+  //city
+  let cityName = response.data.name;
+  let city = document.querySelector("#city");
+  city.innerHTML = cityName;
+
+  //temperature
+  celsiusTemperature = response.data.main.temp;
+
+  let cityTemperature = Math.round(celsiusTemperature);
+  let temperature = document.querySelector("#temperature-number");
+  temperature.innerHTML = cityTemperature;
+
+  //units
+  let celcSign = document.querySelector("#basic-temp-sign");
+  celcSign.innerHTML = `°C`;
+  let fahrSign = document.querySelector("#convert-temp-link");
+  fahrSign.innerHTML = `°F`;
+
+  //weather description
+  let cityDescription = response.data.weather[0].description;
+  let description = document.querySelector("#description");
+  description.innerHTML = cityDescription;
+
+  //weather icon
+  let weatherPicture = response.data.weather[0].icon;
+  let picture = document.querySelector("#weather-icon");
+  picture.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${weatherPicture}@2x.png`
+  );
+
+  getForecast(response.data.coord);
+}
+
+function searchCity(city) {
+  let apiKey = "b107f0128f12cb3797262a88dde7c0fa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showCityData);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input").value; //input
+  searchCity(cityInput);
+}
+
+let city = document.querySelector("#search-form");
+city.addEventListener("submit", handleSubmit); //changes in form
+
+searchCity("Wexford");
+
+// Date--------------------------------------------------------------------
 function formatDate(date) {
   let days = [
     "Sunday",
@@ -25,7 +82,65 @@ let currentTime = new Date();
 let todayDayTime = document.querySelector("#current-day-time");
 todayDayTime.innerHTML = formatDate(currentTime);
 
-//forecast
+// Current Location button--------------------------------------------------
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "eeeac55847ebc465f9151518c7d7b9b6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showCityData);
+}
+
+function showCurrentCity(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentCityButton = document.querySelector("#current-button");
+currentCityButton.addEventListener("click", showCurrentCity);
+
+// Unit conversion----------------------------------------------------------
+
+function showCelsius() {
+  let tempNumber = document.querySelector("#temperature-number");
+  tempNumber.innerHTML = Math.round(celsiusTemperature);
+
+  let celcSign = document.querySelector("#basic-temp-sign");
+  celcSign.innerHTML = `°C`;
+  let fahrSign = document.querySelector("#convert-temp-link");
+  fahrSign.innerHTML = `°F`;
+}
+
+function showFahrenheit() {
+  let convertedTemperature = Math.round((celsiusTemperature * 9) / 5 + 32);
+  let tempNumber = document.querySelector("#temperature-number");
+  tempNumber.innerHTML = convertedTemperature;
+
+  let fahrSign = document.querySelector("#basic-temp-sign");
+  fahrSign.innerHTML = `°F`;
+  let celcSign = document.querySelector("#convert-temp-link");
+  celcSign.innerHTML = `°C`;
+}
+
+function convertTemperature(event) {
+  event.preventDefault();
+  clickCount++;
+
+  if (clickCount % 2 === 0) {
+    showCelsius();
+  } else {
+    showFahrenheit();
+  }
+}
+
+let celsiusTemperature = null;
+let convertLink = document.querySelector("#convert-temp-link");
+let clickCount = 0;
+convertLink.addEventListener("click", convertTemperature);
+
+// ---------------------------------------------------------------------------      Forecast
+//---------------------------------------------------------------------------
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -83,112 +198,3 @@ function getForecast(coordinates) {
 
   axios.get(apiUrl).then(displayForecast);
 }
-
-// Week 5
-// In your project, when a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.
-function showCityData(response) {
-  clickCount = 0;
-
-  let cityName = response.data.name;
-  let city = document.querySelector("#city");
-  city.innerHTML = cityName;
-
-  celsiusTemperature = response.data.main.temp;
-
-  let cityTemperature = Math.round(celsiusTemperature);
-  let temperature = document.querySelector("#temperature-number");
-  temperature.innerHTML = cityTemperature;
-
-  let celcSign = document.querySelector("#basic-temp-sign");
-  celcSign.innerHTML = `°C`;
-  let fahrSign = document.querySelector("#convert-temp-link");
-  fahrSign.innerHTML = `°F`;
-
-  let cityDescription = response.data.weather[0].description;
-  let description = document.querySelector("#description");
-  description.innerHTML = cityDescription;
-
-  let weatherPicture = response.data.weather[0].icon;
-  let picture = document.querySelector("#weather-icon");
-  picture.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${weatherPicture}@2x.png`
-  );
-
-  getForecast(response.data.coord);
-}
-
-function searchCity(city) {
-  let apiKey = "b107f0128f12cb3797262a88dde7c0fa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showCityData);
-}
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input").value; //input
-  searchCity(cityInput);
-}
-
-let city = document.querySelector("#search-form");
-city.addEventListener("submit", handleSubmit);
-
-searchCity("Wexford");
-
-// 🙀 Bonus point:
-// Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.
-
-function showPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "eeeac55847ebc465f9151518c7d7b9b6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showCityData);
-}
-
-function showCurrentCity(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-let currentCity = document.querySelector("#current-button");
-currentCity.addEventListener("click", showCurrentCity);
-
-// 🙀Bonus Feature Week 4
-// Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
-
-function showCelsius() {
-  let tempNumber = document.querySelector("#temperature-number");
-  tempNumber.innerHTML = Math.round(celsiusTemperature);
-
-  let celcSign = document.querySelector("#basic-temp-sign");
-  celcSign.innerHTML = `°C`;
-  let fahrSign = document.querySelector("#convert-temp-link");
-  fahrSign.innerHTML = `°F`;
-}
-
-function showFahrenheit() {
-  let convertedTemperature = Math.round((celsiusTemperature * 9) / 5 + 32);
-  let tempNumber = document.querySelector("#temperature-number");
-  tempNumber.innerHTML = convertedTemperature;
-
-  let celcSign = document.querySelector("#basic-temp-sign");
-  celcSign.innerHTML = `°F`;
-  let fahrSign = document.querySelector("#convert-temp-link");
-  fahrSign.innerHTML = `°C`;
-}
-
-function convertTemperature(event) {
-  event.preventDefault();
-  clickCount++;
-
-  if (clickCount % 2 === 0) {
-    showCelsius();
-  } else {
-    showFahrenheit();
-  }
-}
-
-let celsiusTemperature = null;
-let convertLink = document.querySelector("#convert-temp-link");
-let clickCount = 0;
-convertLink.addEventListener("click", convertTemperature);
